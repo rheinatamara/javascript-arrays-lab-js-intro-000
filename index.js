7f3d2f0cfad8027ff25aafc26efd03f64b28a797
Rheina <rheina.tamara@outlook.com>
2020-11-28 18:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 
 function destructivelyAppendKitten(name){
-  return kittens.push(name);
+    return kittens.push(name)
 }
 
-console.log(destructivelyAppendKitten("Ralph");
+function destructivelyPrependKitten(name){
+    return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+    return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+    return kittens.shift()
+}
+
+function appendKitten(name){
+    return [...kittens, name]
+}
+
+function prependKitten(name){
+    return [name, ...kittens]
+}
+
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+    return kittens.slice(1)
+}
