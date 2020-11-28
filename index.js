7508a3b0f791e0dc5dc5bde9971582922839c6e5
Rheina <rheina.tamara@outlook.com>
2020-11-28 17:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  const newArray = kittens.concat("momo")
+  return newArray;
+}
+console.log(destructivelyAppendKitten(kittens));
